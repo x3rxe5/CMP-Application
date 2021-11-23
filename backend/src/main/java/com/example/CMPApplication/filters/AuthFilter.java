fe1a62ca8aa3f0c638a1905c69ab6664a8fc1159
xerxes <sharmapulkit310@gmail.com>
2021-11-23 23:36:12 +0530

    Jwt String extracted from Cookie

--- backend/src/main/java/com/example/CMPApplication/filters/AuthFilter.java
+++ backend/src/main/java/com/example/CMPApplication/filters/AuthFilter.java
@@ -4,30 +4,37 @@ import com.example.CMPApplication.Constants;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 public class AuthFilter extends GenericFilterBean {
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         String authHeader = request.getHeader("Authorization");
+        Cookie name = WebUtils.getCookie(request, "jwt");
+
         if(authHeader != null){
             String authHeaderArr[] = authHeader.split("Bearer ");
             if (authHeaderArr != null && authHeaderArr.length > 1){
                 String token = authHeaderArr[1];
                 try{
-                    Claims claims = Jwts.parser().setSigningKey(Constants.API_SECRET_KEY).parseClaimsJws(token).getBody();
-                    request.setAttribute("userId",Integer.parseInt(claims.get("userId").toString()));
+                    parsingToken(request,token);
                 }catch(Exception e){
                     response.sendError(HttpServletResponse.SC_FORBIDDEN,"Invalid Token");
                     return;
@@ -37,10 +44,24 @@ public class AuthFilter extends GenericFilterBean {
                 return;
             }
         }else{
-            response.sendError(HttpServletResponse.SC_FORBIDDEN,"Authorization header must be provided");
+            // parsing jwt string with Cookie and set to parsingToken()
+            if (name != null) {
+                parsingToken(request,name.getValue());
+            } else {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN,"Authorization header must be provided");
+            }
             return;
         }
 
         filterChain.doFilter(request,response);
+    }
+
+    public void parsingToken(ServletRequest servletRequest, String token){
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        Claims claims = Jwts.parser().setSigningKey(Constants.API_SECRET_KEY).parseClaimsJws(token).getBody();
+        request.setAttribute("userId",Integer.parseInt(claims.get("userId").toString()));
+
     }
 }
